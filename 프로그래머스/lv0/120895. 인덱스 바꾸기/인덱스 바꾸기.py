924988e26736ed5a8098d6ce0b69241ea071e0b1
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2023-08-12 19:09:41 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,7 +1,5 @@
 def solution(my_string, num1, num2):
-    ans = my_string[:num1]
-    ans += my_string[num2]
-    ans += my_string[num1 + 1 : num2]
-    ans += my_string[num1]
-    ans += my_string[num2 + 1: ]
+    my_string = list(my_string) # iterable object로 변경
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1] # 자리 변경
+    ans = ''.join(my_string)
     return ans

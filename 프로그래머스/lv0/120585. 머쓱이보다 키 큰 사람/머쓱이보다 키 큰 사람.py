3405a53589d38849b0e135882947d9fd441f08d1
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2023-08-14 17:06:30 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,9 @@
+# 비효율적인 코드: sorting한 순간 최소 O(n log n)이 된다
+# array.append(height) # 기존 리스트에 머쓱이 키를 추가
+# dic = {} # 빈 딕셔너리 만들기
+# for idx, tall in enumerate(sorted(array)): # 정렬한 리스트에서 인덱스와 키 조합을
+#     dic[tall] = idx                        # 딕셔너리에 key와 value로 추가
+# return len(array) - (dic[height] + 1) # 전체 인원의 수 - 머쓱이의 순서
+
 def solution(array, height):
-    array.append(height) # 기존 리스트에 머쓱이 키를 추가
-    dic = {} # 빈 딕셔너리 만들기
-    for idx, tall in enumerate(sorted(array)): # 정렬한 리스트에서 인덱스와 키 조합을
-        dic[tall] = idx                        # 딕셔너리에 key와 value로 추가
-    return len(array) - (dic[height] + 1) # 전체 인원의 수 - 머쓱이의 순서
+    return sum(1 for i in array if i > height)

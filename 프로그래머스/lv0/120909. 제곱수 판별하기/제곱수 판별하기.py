13e0627b6a8f2943dba86a5bc0a9dcc39e10c19f
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2023-08-14 16:45:41 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,9 +1,2 @@
 def solution(n):
-    i = 1
-    while i <= n:
-        if i * i == n:
-            return 1
-        else:
-            i += 1
-    return 2
-    
+    return 1 if (n ** 0.5) % 1 == 0 else 2

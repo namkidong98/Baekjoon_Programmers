b5a3aa4040a5285365cc6018cc808b319e09eaab
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2023-10-02 22:55:35 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 108 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline  # input 함수를 재정의
+#stack = deque() # deque 객체로 stack 생성
+
+while True :
+    a = input().rstrip()
+    stack = []
+
+    if a == "." :
+        break
+
+    for i in a :
+        if i == '[' or i == '(' :
+            stack.append(i)
+        elif i == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop() # 맞으면 지워서 stack을 비워줌 0 = yes
+            else : 
+                stack.append(']')
+                break
+        elif i == ')' :
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+    if len(stack) == 0 :
+        print('yes')
+    else :
+        print('no')
